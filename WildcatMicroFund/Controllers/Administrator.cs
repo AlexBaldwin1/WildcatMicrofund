f8d65bd63810d6c29b01870580cde5663c2c8e21
Jason Hansen <jasonhansen10@mail.weber.edu>
2020-07-08 20:29:39 -0600

    Bug fixes

--- WildcatMicroFund/Controllers/Administrator.cs
+++ WildcatMicroFund/Controllers/Administrator.cs
@@ -3,14 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WildcatMicroFund.Data.Context;
+using WildcatMicroFund.Data.Models;
 
 namespace WildcatMicroFund.Controllers
 {
     public class Administrator : Controller
     {
-        public IActionResult Index()
+        private readonly WildcatMicroFundDatabaseContext _context;
+
+        public Administrator(WildcatMicroFundDatabaseContext context)
         {
+            _context = context;
+        }
+
+        /*        public IActionResult Index()
+                {
+                    return View();
+                }*/
+
+        public async Task<IActionResult> Index()
+        {
+            //return new Task(Task.Run(a => new View());
             return View();
+        }
+
+
+        public async Task<IActionResult> Users()
+        {
+            var user = _context.Users;
+/*
+                .Include(u => u.FirstName)
+                .Include(u => u.Email)
+                .Include(u => u.UserRoles);*/
+
+                return View(user);
+
         }
     }
 }
